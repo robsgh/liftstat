f938f5e0ab5a9950634ae23aeb93f301dc4a64ac
Robert Schmidt <rob+git@schminfra.net>
2026-03-01 13:19:37 -0800

    Initial Commit

---
+++ LiftStat/LiftStatApp.swift
@@ -0,0 +1,32 @@
+//
+//  LiftStatApp.swift
+//  LiftStat
+//
+//  Created by Rob Schmidt on 3/1/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct LiftStatApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ LiftStatTests/LiftStatTests.swift
@@ -0,0 +1,17 @@
+//
+//  LiftStatTests.swift
+//  LiftStatTests
+//
+//  Created by Rob Schmidt on 3/1/26.
+//
+
+import Testing
+@testable import LiftStat
+
+struct LiftStatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
